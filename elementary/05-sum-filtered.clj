04ca133f6271cb9d0998781132f24dba4e8e5b80
Rhys Roberts <rhysr@mega-city-one.co.uk>
2013-12-11 20:08:21 +0000

    Change filtered sum to use reduce

--- elementary/05-sum-filtered.clj
+++ elementary/05-sum-filtered.clj
@@ -4,25 +4,21 @@
 (defn looks-like-an-int? [n]
   (re-find #"^\d+$" n))
 
-(defn divisible-by-3-or-5? [n] 
-  (or 
-    (= 0 (mod n 3)) 
+(defn divisible-by-3-or-5? [n]
+  (or
+    (= 0 (mod n 3))
     (= 0 (mod n 5))))
 
-(defn sum-to-zero
-  ([can-sum? n]
-    (sum-to-zero can-sum? n 0))
-  ([can-sum? n sum]
-    (if (= n 0)
-      sum
-      (if (can-sum? n)
-        (recur can-sum? (dec n) (+ n sum))
-        (recur can-sum? (dec n) sum)))))
-
-(defn sum-one-to-n [can-sum? n]
-  (if (not (looks-like-an-int? n))
-    (println "not a positive integer")
-    (sum-to-zero can-sum? (convert-to-int n))))
 
 (println "Please enter a number: ")
-(println (sum-one-to-n divisible-by-3-or-5? (read-line)))
+(println
+  (let [n (read-line)]
+    (if (not (looks-like-an-int? n))
+      "not a positive integer"
+      (reduce
+        +
+        (filter
+          divisible-by-3-or-5?
+          (range
+            1
+            (+ 1 (convert-to-int n)))))))) ;range upper bound is exclusive
