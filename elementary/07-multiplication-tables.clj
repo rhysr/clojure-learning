cb6727d0e42334ac1ab9c59e32b336d26f17d57b
Rhys Roberts <rhysr@mega-city-one.co.uk>
2013-12-14 12:45:12 +0000

    Multiplication tables in nested loops

--- elementary/07-multiplication-tables.clj
+++ elementary/07-multiplication-tables.clj
@@ -1,10 +1,12 @@
 (defn table-row [x y]
   (println (str x " * " y " = " (* x y))))
 
-(doseq
-  [nums (for [x (range 1 13)
-              y (range 1 13)]
-          [x y])]
-  (table-row
-    (first nums)
-    (second nums)))
+(loop [x 1 y 1]
+  (table-row x y)
+  (if
+    (= 12 x)
+    (if
+      (= 12 y)
+      nil
+      (recur 1 (inc y)))
+    (recur (inc x) y)))
